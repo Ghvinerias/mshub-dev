cbc9fe9df5d54c6161954b5c493b21b6e0b3a3e2
mshubitidze <59510085+mshubitidze@users.noreply.github.com>
2023-10-04 22:07:47 +0400

    add redirects

---
+++ middleware.ts
@@ -0,0 +1,34 @@
+import { NextResponse, type NextRequest } from "next/server";
+
+export default async function middleware(req: NextRequest) {
+  const host = req.nextUrl.origin.split("://")[1];
+
+  if (
+    host.includes("gh.piss.ge") ||
+    host.includes("gh.mshub.dev") ||
+    host.includes("gh.msh.ge")
+  )
+    return NextResponse.redirect("https://github.com/mshubitidze");
+
+  if (
+    host.includes("x.piss.ge") ||
+    host.includes("x.mshub.dev") ||
+    host.includes("x.msh.ge")
+  )
+    return NextResponse.redirect("https://x.com/_mshub");
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+  ],
+};
