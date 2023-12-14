2424f32ad46672f17323bcfc998c07a207cd7b26
Aleksandre Ghvineria <Ghvinerias@gmail.com>
2023-12-14 16:36:20 +0000

    fix

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
 
-module.exports = nextConfig
+}
+
+module.exports = {
+    output: 'export',
+    // other configurations...
+  };
